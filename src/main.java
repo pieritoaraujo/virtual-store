3ae59989a1193ff79195eb468851e3e27270fe4f
Piero Araujo <pieritoeeuu@gmail.com>
2026-05-23 18:45:38 -0500

    feat: agregar clase main e importar packages

--- src/main.java
+++ src/main.java
@@ -1,3 +1,15 @@
-void main() {
-    
+import model.Product;
+import model.Cart;
+import adapter.ExternalPaypalService;
+import adapter.PayPalAdapter;
+import adapter.PaymentProcessor;
+import observer.EmailNotificationObserver;
+import observer.AdminNotificationObserver;
+import observer.InventoryObserver;
+import service.OrderService;
+
+class main {
+    static void main(String[] args) {
+
+    }
 }

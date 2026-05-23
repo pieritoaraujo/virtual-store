df065c4ddb190560014ce5c231bfacfb4d382efc
Piero Araujo <pieritoeeuu@gmail.com>
2026-05-23 18:28:54 -0500

    feat: agregar clase OrderService

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,4 +1,15 @@
 package service;
+    import observer.OrderObserver;
+    import java.util.ArrayList;
+    import java.util.List;
 
 public class OrderService {
+    private List<OrderObserver> observers = new ArrayList<>();
+
+    public void agregarObserver(OrderObserver observer) {
+        observers.add(observer);
+    }
+    public void notificarObserver(String message){
+        for(OrderObserver observer : observers);
+    }
 }

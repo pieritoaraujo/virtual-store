84b634fa22b3c0b7a86c871741f8ccfa8138781c
Piero Araujo <pieritoeeuu@gmail.com>
2026-05-23 17:21:04 -0500

    feat: agregar el model Product y Cart

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,4 +1,19 @@
 package model;
-
+    import java.util.List;
+    import java.util.ArrayList;
 public class Cart {
+    private List<Product> producto = new ArrayList<>();
+
+    public void agregarProducto(Product p){
+        producto.add(p);
+    }
+
+    public double calculoTotal(){
+        double Ctotal = 0;
+
+        for (Product p : producto){
+            Ctotal += p.getPrecio();
+        }
+        return Ctotal;
+    }
 }
--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Product {
+    private String nombre;
+    private double precio;
+
+    public Product(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public double getPrecio() {
+        return precio;
+    }
 }

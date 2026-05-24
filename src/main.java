cd6c29314f9a17b15ec8d123cbbd340c5cd9e0c6
Piero Araujo <pieritoeeuu@gmail.com>
2026-05-23 19:05:25 -0500

    feat: Implementando strategy, adapter y observer en la clase main

--- src/main.java
+++ src/main.java
@@ -11,5 +11,26 @@ import service.OrderService;
 class main {
     static void main(String[] args) {
 
+        Product p1 = new Product("Play Station", 2599);
+        Product p2 = new Product("Xbox", 2450);
+        Product p3 = new Product("Laptop Gamer", 7600);
+        Product p4 = new Product("Television 75", 1800);
+        Product p5 = new Product("Silla Gamer", 550);
+
+        Cart carrocomprasPA = new Cart();
+        carrocomprasPA.agregarProducto(p1);
+        carrocomprasPA.agregarProducto(p5);
+
+        double total = carrocomprasPA.calculoTotal();
+
+        PaymentProcessor pago = new PayPalAdapter(new ExternalPaypalService());
+        OrderService OrdenS = new OrderService();
+        OrdenS.agregarObserver(new EmailNotificationObserver());
+        OrdenS.agregarObserver(new InventoryObserver());
+        OrdenS.agregarObserver(new AdminNotificationObserver());
+
+        System.out.println("Compra confirmada por S/ " +total);
+
+        OrdenS.notificarObserver("Compra realizada");
     }
 }
